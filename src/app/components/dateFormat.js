e30e19dc77f7139b0d155809603ff55be247dea1
jaimeferreira11 <jaimeferreira11@gmail.com>
2018-01-16 08:08:57 -0300

    WIP.Add date component React

---
+++ src/app/components/dateFormat.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import DatePicker from 'react-datepicker';
+import moment from 'moment';
+
+
+class DateFormat extends React.Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      startDate: moment(),
+      title: props.schema.title,
+      required: props.required ? '*' : '',
+    };
+    this.handleChange = this.handleChange.bind(this);
+  }
+
+  handleChange(date) {
+    this.setState({
+      startDate: date
+    });
+  }
+
+  render() {
+    return(
+
+      <div className="row">
+        <div className="col-sm-12 ">
+          <label className="control-label gmap-label">
+            {this.state.title} {this.state.required}
+          </label>
+
+          <DatePicker
+            selected={this.state.startDate}
+            className="form-control datepicker"
+            onChange={this.handleChange}
+            showYearDropdown
+            showMonthDropdown
+            dropdownMode="select"
+            withPortal
+          />
+
+        </div>
+      </div>
+
+
+
+  );
+  }
+}
+export default DateFormat;
