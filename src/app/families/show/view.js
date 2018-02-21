a53ad70b636c4ef31d03347170c26c2d27e4aa95
jaimeferreira11 <jaimeferreira11@gmail.com>
2018-02-21 16:41:11 -0300

    View only priorities in family dashboard

--- src/app/families/show/view.js
+++ src/app/families/show/view.js
@@ -13,6 +13,7 @@ export default Mn.View.extend({
   initialize(options) {
     this.app = options.app;
     this.entity = options.entity;
+
   },
   onRender() {
     const headerItems = storage.getSubHeaderItems(this.model);
